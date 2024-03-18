97b70a041af736101d2e6ce69b474813b438b5f5
floh-bhaptics <floh@fahrenbergers.de>
2024-03-18 13:38:28 +0100

    Add project files.

---
+++ MyOWOVest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Harmony;
+using MelonLoader;
+using OWOGame;
+//using MyOWOSensations;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        public Dictionary<String, Sensation> FeedbackMap = new Dictionary<String, Sensation>();
+
+        public TactsuitVR()
+        {
+            RegisterAllTactFiles();
+            InitializeOWO();
+        }
+
+        private async void InitializeOWO()
+        {
+            LOG("Initializing suit");
+
+            // New auth.
+            //var gameAuth = GameAuth.Create(AllBakedSensations()).WithId("79724427");
+            var gameAuth = GameAuth.Create(AllBakedSensations());
+
+            OWO.Configure(gameAuth);
+            string[] myIPs = getIPsFromFile("OWO_Manual_IP.txt");
+            if (myIPs.Length == 0) await OWO.AutoConnect();
+            else
+            {
+                await OWO.Connect(myIPs);
+            }
+
+            if (OWO.ConnectionState == ConnectionState.Connected)
+            {
+                suitDisabled = false;
+                LOG("OWO suit connected.");
+            }
+            if (suitDisabled) LOG("Owo is not enabled?!?!");
+        }
+
+        public string[] getIPsFromFile(string filename)
+        {
+            List<string> ips = new List<string>();
+            string filePath = Directory.GetCurrentDirectory() + "\\Mods\\" + filename;
+            if (File.Exists(filePath))
+            {
+                LOG("Manual IP file found: " + filePath);
+                var lines = File.ReadLines(filePath);
+                foreach (var line in lines)
+                {
+                    if (IPAddress.TryParse(line, out _)) ips.Add(line);
+                    else LOG("IP not valid? ---" + line + "---");
+                }
+            }
+            return ips.ToArray();
+        }
+
+        ~TactsuitVR()
+        {
+            LOG("Destructor called");
+            DisconnectOwo();
+        }
+
+        private BakedSensation[] AllBakedSensations()
+        {
+            var result = new List<BakedSensation>();
+
+            foreach (var sensation in FeedbackMap.Values)
+            {
+                if (sensation is BakedSensation baked)
+                {
+                    LOG("Registered baked sensation: " + baked.name);
+                    result.Add(baked);
+                }
+                else
+                {
+                    LOG("Sensation not baked? " + sensation);
+                    continue;
+                }
+            }
+            return result.ToArray();
+        }
+
+        public void DisconnectOwo()
+        {
+            LOG("Disconnecting Owo skin.");
+            OWO.Disconnect();
+        }
+
+        public void LOG(string logStr)
+        {
+            MelonLogger.Msg(logStr);
+        }
+
+        void RegisterAllTactFiles()
+        {
+
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\OWO";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.owo", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    Sensation test = Sensation.Parse(tactFileStr);
+                    FeedbackMap.Add(prefix, test);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+            }
+
+            systemInitialized = true;
+        }
+
+
+        public void PlayBackHit(float xzAngle, float yShift)
+        {
+            /*
+            OWOSensation sensation = OWOSensation.ShotEntry;
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            if ((xzAngle < 90f))
+            {
+                if (yShift >= 0f) OWO.Send(sensation, OWOMuscle.Pectoral_Right);
+                else OWO.Send(sensation, OWOMuscle.Abdominal_Right);
+            }
+            if ((xzAngle > 90f) && (xzAngle < 180f))
+            {
+                if (yShift >= 0f) OWO.Send(sensation, OWOMuscle.Dorsal_Right);
+                else OWO.Send(sensation, OWOMuscle.Lumbar_Right);
+            }
+            if ((xzAngle > 180f) && (xzAngle < 270f))
+            {
+                if (yShift >= 0f) OWO.Send(sensation, OWOMuscle.Dorsal_Left);
+                else OWO.Send(sensation, OWOMuscle.Lumbar_Left);
+            }
+            if ((xzAngle > 270f))
+            {
+                if (yShift >= 0f) OWO.Send(sensation, OWOMuscle.Pectoral_Right);
+                else OWO.Send(sensation, OWOMuscle.Abdominal_Right);
+            }
+            */
+            if ((xzAngle < 180f))
+            {
+                PlayBackFeedback("Hit_Front");
+            }
+            else PlayBackFeedback("Hit_Back");
+        }
+
+        public void Recoil(bool isRightHand, bool isTwoHanded)
+        {
+            if (isTwoHanded)
+            {
+                PlayBackFeedback("Recoil_both");
+                return;
+            }
+            if (isRightHand) PlayBackFeedback("Recoil_R");
+            else PlayBackFeedback("Recoil_L");
+        }
+
+        public void PlayBackFeedback(string feedback)
+        {
+            if (FeedbackMap.ContainsKey(feedback))
+            {
+                OWO.Send(FeedbackMap[feedback]);
+            }
+            else LOG("Feedback not registered: " + feedback);
+        }
+
+    }
+}
---
+++ Underdogs_OWO.cs
@@ -0,0 +1,96 @@
+﻿using MelonLoader;
+using MyBhapticsTactsuit;
+using HarmonyLib;
+using Il2Cpp;
+
+[assembly: MelonInfo(typeof(Underdogs_OWO.Underdogs_OWO), "Underdogs_OWO", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonGame("One Hamsa", "UNDERDOGS")]
+
+
+namespace Underdogs_OWO
+{
+    public class Underdogs_OWO : MelonMod
+    {
+        public static TactsuitVR tactsuitVr = null!;
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+        }
+
+        [HarmonyPatch(typeof(PlayerMech), "ShakeCockpit", new Type[] { typeof(float), typeof(float) })]
+        public class bhaptics_ShakeCockpit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerMech __instance)
+            {
+                tactsuitVr.PlayBackFeedback("CockpitShake");
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerMech), "DetachLeft", new Type[] { })]
+        public class bhaptics_DetachLeft
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerMech __instance)
+            {
+                tactsuitVr.PlayBackFeedback("DetachArm_L");
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerMech), "DetachRight", new Type[] { })]
+        public class bhaptics_DetachRight
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerMech __instance)
+            {
+                tactsuitVr.PlayBackFeedback("DetachArm_R");
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerPunchBehavior), "OnPunchResolved", new Type[] { typeof(EntityInteraction) })]
+        public class bhaptics_OnPunchResolved
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerPunchBehavior __instance, EntityInteraction punchInteraction)
+            {
+                float intensity = (0.6f + punchInteraction.result.totalImpact / 60f * 0.4f);
+                if (__instance.mechArm.side == VRSide.Right) tactsuitVr.PlayBackFeedback("Punch_R");
+                else tactsuitVr.PlayBackFeedback("Punch_L");
+            }
+        }
+
+        [HarmonyPatch(typeof(BashGuardEntity), "OnBashResolved", new Type[] { typeof(EntityInteraction) })]
+        public class bhaptics_OnBashResolved
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BashGuardEntity __instance, EntityInteraction bashInteraction)
+            {
+                if (bashInteraction.totalHealthDamage > 0f) tactsuitVr.PlayBackFeedback("Bash");
+                if (bashInteraction.hasImpact) tactsuitVr.PlayBackFeedback("Bash");
+                if (bashInteraction.result.totalImpact > 0f) tactsuitVr.PlayBackFeedback("Bash");
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerMech), "OnChassisHealthChange", new Type[] { typeof(IHealthComponent), typeof(int), typeof(IEntityBehavior) })]
+        public class bhaptics_HealthChanged
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerMech __instance, IHealthComponent healthcomponent)
+            {
+                if (healthcomponent.health <= healthcomponent.maxHealth * 0.25f) tactsuitVr.PlayBackFeedback("ThreeHeartBeats");
+            }
+        }
+
+        [HarmonyPatch(typeof(Bomb), "Explode", new Type[] { })]
+        public class bhaptics_BombBExplode
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Bomb __instance)
+            {
+                tactsuitVr.PlayBackFeedback("Explosion");
+            }
+        }
+
+    }
+}
